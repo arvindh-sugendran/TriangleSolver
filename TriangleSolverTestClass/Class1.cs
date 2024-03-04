1f34bb1efe065996b9bac445e672cd49df3cd157
Arvindh Sugendran <asugendran3300@conestogac.on.ca>
2024-03-03 20:25:04 -0500

    Unit Test for Isosceles triangle completed

--- TriangleSolverTestClass/Class1.cs
+++ TriangleSolverTestClass/Class1.cs
@@ -27,5 +27,60 @@ namespace TriangleSolverTestClass
             }
 
         }
+
+        public class TriangleSolverTestClassForIscoscelesTriangle
+        {
+            [Test]
+            public void AnalyzeTriangle_WithInput6and6and10_OutputValidIsoscelesTriangle()
+            {
+                // Arrange
+                int firstSide = 6;
+                int secondSide = 6;
+                int thirdSide = 10;
+
+                string expected = "A triangle is formed and it is an ISOSCELES";
+
+                // Act
+                string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+                // Assert
+                ClassicAssert.AreEqual(expected, actual);
+            }
+
+
+            [Test]
+            public void AnalyzeTriangle_WithInput7and8and8_OutputValidIsoscelesTriangle()
+            {
+                // Arrange
+                int firstSide = 7;
+                int secondSide = 8;
+                int thirdSide = 8;
+
+                string expected = "A triangle is formed and it is an ISOSCELES";
+
+                // Act
+                string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+                // Assert
+                ClassicAssert.AreEqual(expected, actual);
+            }
+
+            [Test]
+            public void AnalyzeTriangle_WithInput9and5and9_OutputValidIsoscelesTriangle()
+            {
+                // Arrange
+                int firstSide = 9;
+                int secondSide = 5;
+                int thirdSide = 9;
+
+                string expected = "A triangle is formed and it is an ISOSCELES";
+
+                // Act
+                string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+                // Assert
+                ClassicAssert.AreEqual(expected, actual);
+            }
+        }
     }
 }
