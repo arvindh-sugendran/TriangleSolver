68cd632f8e6b4fdc4385e13cb206769b8e620847
Arvindh Sugendran <asugendran3300@conestogac.on.ca>
2024-03-03 19:24:42 -0500

    Unit Test for Equilateral triangle completed

--- TriangleSolverTestClass/Class1.cs
+++ TriangleSolverTestClass/Class1.cs
@@ -6,6 +6,26 @@ namespace TriangleSolverTestClass
 {
     public class TriangleSolverTestClass
     {
+        [TestFixture]
+        public class TriangleSolverTestClassForEquilateralTriangle
+        {
+            [Test]
+            public void AnalyzeTriangle_WithInput6and6and6_OutputValidEquilateralTriangle()
+            {
+                // Arrange
+                int firstSide = 6;
+                int secondSide = 6;
+                int thirdSide = 6;
 
+                string expected = "A triangle is formed and it is an EQUILATERAL";
+
+                // Act
+                string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+                // Assert
+                ClassicAssert.AreEqual(expected, actual);
+            }
+
+        }
     }
 }
